becfb989eb0cd3f12b8c44678c5e161770d8d0d3
salban <salban.nithilaselvan@gmail.com>
2023-01-26 13:57:55 -0500

    field inspection

--- TeamCode/src/main/java/teamcode/v1/constants/OdoConstants.kt
+++ TeamCode/src/main/java/teamcode/v1/constants/OdoConstants.kt
@@ -4,6 +4,6 @@ import com.acmerobotics.dashboard.config.Config
 
 @Config
 object OdoConstants {
-    @JvmField var TRACK_WIDTH = 6.5
-    @JvmField var PERP_TRACKER = 3.652
+    @JvmField var TRACK_WIDTH = 6.52
+    @JvmField var PERP_TRACKER = 3.2
 }
